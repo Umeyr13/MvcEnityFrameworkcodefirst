dceb9e440f5fe0d0133836f823e185e22b21201a
umeyr13 <umeyr.gulcimen@gmail.com>
2023-04-06 13:19:36 +0300

    Kişi ve adres için "Ekle,Sil Düzenle"

---
+++ MvcEnityFrameworkcodefirst/Controllers/AdresController.cs
@@ -0,0 +1,104 @@
+﻿using MvcEnityFrameworkcodefirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEnityFrameworkcodefirst.Controllers
+{
+    public class AdresController : Controller
+    {
+            DatabaseContext db = new DatabaseContext();
+
+        // GET: Adres
+        public ActionResult Yeni()
+        {
+            List<Kişiler> kisiler = db.Kişiler.ToList();
+            List<SelectListItem> liste = new List<SelectListItem>();
+            //foreach (var item in kisiler)
+            //{
+            //    liste.Add(new SelectListItem()
+            //    {
+            //        Text = item.Ad + " " + item.Soyad,
+            //        Value = item.ID.ToString()
+
+            //    });
+
+
+            //}
+
+            //Linq İle
+            liste = (from kisi in kisiler select new SelectListItem() 
+            {
+             
+                Text = kisi.Ad + " " + kisi.Soyad,Value = kisi.ID.ToString()
+                
+            }).ToList();
+            ViewBag.kisiler = liste; //view bag bir kere gösterir listeyi sonra silinir.
+            TempData["liste"] = liste; //lissteyi hemen silinmesin diye temp data ya attık. 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Yeni(Adresler adres)
+        {
+          Kişiler kisi =  db.Kişiler.Where(x => x.ID == adres.kisi.ID).FirstOrDefault();
+            if (kisi!=null)
+            {
+                adres.kisi = kisi;
+                db.Adresler.Add(adres);
+               int sonuc = db.SaveChanges();
+                if (sonuc > 0) { ViewBag.mesaj = "Adres kaydedildi"; ViewBag.renk = "success"; }
+
+                else           { ViewBag.mesaj = "Adres kaydedilemedi."; ViewBag.renk = "danger"; }
+
+            }
+            ViewBag.kisiler = TempData["liste"];// Yeni listeyi view e bag tekrar temp data ile attık. ViewBag.kisiler in içi boşaldı tek seferlik di o . temp data olmasa veri tabanına bağlanıp tekrar liste almamız gerekirdi.
+            return View();
+        }
+
+        public ActionResult Duzenle(int? adresId)
+        {
+            Adresler adres = null;
+            if (adresId != null)
+            {
+                adres = db.Adresler.Where(x => x.ID == adresId).FirstOrDefault();
+                List<SelectListItem> liste = (from kisi in db.Kişiler.ToList()
+                select new SelectListItem()
+                {
+                    Text = kisi.Ad + " " + kisi.Soyad,
+                    Value = kisi.ID.ToString()
+
+                }).ToList();
+                ViewBag.kisiler = liste;
+                TempData["liste"] = liste;
+            }
+
+            return View(adres);
+
+        }
+
+        [HttpPost]
+        public ActionResult Duzenle(Adresler model,int? adresId)
+        {
+            Adresler adres = db.Adresler.Where(x => x.ID == adresId).FirstOrDefault();
+            Kişiler kisi  = db.Kişiler.Where(x => x.ID == model.kisi.ID).FirstOrDefault();
+
+            if (kisi != null)
+            {
+                adres.Adrestanim = model.Adrestanim;
+                adres.kisi = kisi;
+              
+                int sonuc = db.SaveChanges();
+                if (sonuc > 0) { ViewBag.mesaj = "Adres kaydedildi"; ViewBag.renk = "success"; }
+
+                else { ViewBag.mesaj = "Adres kaydedilemedi."; ViewBag.renk = "danger"; }
+
+            }
+            ViewBag.kisiler = TempData["liste"];
+
+            return View();
+        }
+    }
+}
--- MvcEnityFrameworkcodefirst/Controllers/KisiController.cs
+++ MvcEnityFrameworkcodefirst/Controllers/KisiController.cs
@@ -9,6 +9,8 @@ namespace MvcEnityFrameworkcodefirst.Controllers
 {
     public class KisiController : Controller
     {
+
+        DatabaseContext db = new DatabaseContext();
         // GET: Kisi
         public ActionResult Yeni()
         {
@@ -17,12 +19,81 @@ namespace MvcEnityFrameworkcodefirst.Controllers
         [HttpPost]
         public ActionResult Yeni(Kişiler kisi)
         {
-            DatabaseContext context = new DatabaseContext();
-            context.Kişiler.Add(kisi);
-           int sonuc = context.SaveChanges();
+            
+            db.Kişiler.Add(kisi);
+           int sonuc = db.SaveChanges();
             if (sonuc > 0) { ViewBag.mesaj = "Kişi kaydedildi"; ViewBag.renk ="success"; } 
                       else { ViewBag.mesaj="Kişi kaydedilemedi."; ViewBag.renk ="danger"; }
             return View();
+        }
+
+        public ActionResult Duzenle (int? KisiId)//soruİşareti demek kişi ıd null olabilir demektir.
+        {
+            Kişiler kisi = null;
+            if (KisiId != null)
+            {
+                
+               kisi = db.Kişiler.Where(x => x.ID == KisiId).FirstOrDefault();
+
+            }
+          
+            return View (kisi);
+        }
+
+        [HttpPost]
+        public ActionResult Duzenle(Kişiler model,int? kisiid )
+        {
+            Kişiler kisi = db.Kişiler.Where(x => x.ID == kisiid).FirstOrDefault();
+            if (kisi!= null)
+            {
+                kisi.Ad = model.Ad;
+                kisi.Soyad = model.Soyad;
+                kisi.Yas = model.Yas;
+                int sonuc = db.SaveChanges();
+            if (sonuc >0)
+            {
+                ViewBag.mesaj ="Kişi Bilgileri Güncellendi";
+                    ViewBag.renk = "success";
+            }
+                else
+                {
+                    ViewBag.mesaj = "Kişi Bilgileri Güncellenemedi";
+                    ViewBag.renk = "danger";
+                }
+
+            }
+            return View ();
+        }
+
+        public ActionResult Sil(int? kisiId)
+        {
+            Kişiler kisi = null ;
+            if (kisiId !=null)
+            {
+                kisi = db.Kişiler.Where(x => x.ID == kisiId).FirstOrDefault();
+
+            }
+
+            return View (kisi);
+        }
+
+        [HttpPost,ActionName("Sil")]
+        public ActionResult Sil2(int? kisiID)
+        {
+            if (kisiID != null)
+            {
+              Kişiler kisi = db.Kişiler.Where(x=>x.ID ==kisiID).FirstOrDefault();
+                //List<Adresler> adresler = db.Adresler.Where(x => x.ID == kisiID).ToList();
+                //foreach (var item in adresler)
+                //{
+                //    db.Adresler.Remove(item); // adres ile bağlantısı varsa kişi silmeye izin vermez. böyle durumda istersen önce adresini silip kişi silebilirsin
+                //}
+
+                db.Kişiler.Remove(kisi);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Homepage","Home");//kişi yi silip başka sayfaya yönlendirdik.
+
         }
 
     }
